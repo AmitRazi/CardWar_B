9fd3e0e52ac9dfa4ca160ad52819a62fe90eec78
AmitRazi <115696533+AmitRazi@users.noreply.github.com>
2023-04-13 15:53:00 +0300

    Update game.hpp

--- sources/game.hpp
+++ sources/game.hpp
@@ -17,6 +17,8 @@ namespace ariel {
         int p2Wins;
         int draws;
         int rounds;
+        int cardsWonByPlayer1;
+        int cardsWonByPlayer2;
 
         std::vector<Card> deck;
         std::vector<std::string> playLog;
@@ -31,13 +33,42 @@ namespace ariel {
         void distributeCard();
 
     public:
-        Game(Player &player1, Player &player2);
 
-        Game(const Game &);
 
-        Game(Game &&) noexcept;
+        Game(Player &player1, Player &player2) : player1(player1), player2(player2), p1Wins(0), p2Wins(0), draws(0),
+                                                 rounds(0), cardsWonByPlayer1(0), cardsWonByPlayer2(0) {
+            Game::createDeck();
+            Game::shuffleDeck();
+            Game::distributeCard();
+        }
 
-        ~Game();
+        //Copy constructor
+        Game(const Game &other) : Game(other.player1, other.player2) {
+        }
+
+        //Move constructor
+        Game(Game &&other) noexcept
+                : player1(other.player1),
+                  player2(other.player2),
+                  p1Wins(other.p1Wins),
+                  p2Wins(other.p2Wins),
+                  draws(other.draws),
+                  rounds(other.rounds),
+                  deck(std::move(other.deck)),
+                  cardsWonByPlayer1(other.cardsWonByPlayer1),
+                  cardsWonByPlayer2(other.cardsWonByPlayer2),
+                  playLog(std::move(other.playLog)),
+                  winner(std::move(other.winner)) {
+
+            other.cardsWonByPlayer2 = 0;
+            other.cardsWonByPlayer1 = 0;
+            other.p1Wins = 0;
+            other.p2Wins = 0;
+            other.draws = 0;
+            other.rounds = 0;
+        }
+
+        ~Game() = default;
 
         bool playTurn();
 
