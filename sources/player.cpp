149bd441607af26909d1ccf452b53b8e851d483b
AmitRazi <115696533+AmitRazi@users.noreply.github.com>
2023-04-13 15:53:06 +0300

    Update player.cpp

--- sources/player.cpp
+++ sources/player.cpp
@@ -2,19 +2,6 @@
 
 using namespace ariel;
 
-Player::Player(std::string name) : name(std::move(name)), wonCards(0) {
-    if(this->name.length() == 0) this->name = "No Name";
-}
-
-Player::Player(const Player &other) : Player(other.name) {
-}
-
-Player::Player(Player &&other) noexcept: name(std::move(other.name)), stack(std::move(other.stack)),
-                                         wonCards(other.wonCards) {
-    other.wonCards = -1;
-}
-
-Player::~Player() = default;
 
 std::string Player::getName() {
     return name;
@@ -28,24 +15,30 @@ int Player::cardesTaken() const {
     return wonCards;
 }
 
+//This function is invoked by the game instance with an int argument to indicate the number of cards won by the player.
 void Player::won(int num_of_cards) {
     wonCards += num_of_cards;
 }
 
+//This function is invoked by the game instance with a card argument to be added to the players stack.
 void Player::addCard(const Card &card) {
     stack.push_back(card);
 }
 
+//This function is invoked by the game instance to remove and return the top card in the players stack.
 Card Player::removeCard() {
     Card top = topCard();
     stack.pop_back();
     return top;
 }
 
+//Returns the top card in the players stack.
 Card Player::topCard() const {
     return stack.back();
 }
 
+
+//Overloading the copy operator
 Player &Player::operator=(const Player &other) {
     if (this == &other) {
         return *this;
@@ -58,6 +51,7 @@ Player &Player::operator=(const Player &other) {
     return *this;
 }
 
+//Overloading the Move operator
 Player &Player::operator=(Player &&other) noexcept {
     if (this == &other) {
         return *this;
