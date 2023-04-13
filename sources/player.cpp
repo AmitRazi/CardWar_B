80c2ca9f853c8492f4220ac614364328b98f813e
AmitRazi <115696533+AmitRazi@users.noreply.github.com>
2023-04-13 10:13:15 +0300

    Update player.cpp

--- sources/player.cpp
+++ sources/player.cpp
@@ -1,47 +1,73 @@
-//
-// Created by 97250 on 21/03/2023.
-//
+#include "player.hpp"
 
-#ifndef CARDGAME_PLAYER_HPP
-#define CARDGAME_PLAYER_HPP
+using namespace ariel;
 
-#include "card.hpp"
-
-namespace ariel {
-    class Player {
-    private:
-        std::string name;
-        std::vector<Card> stack;
-        int wonCards;
-
-    public:
-
-        Player(std::string name = "No Name");
-
-        Player(Player &&) noexcept;
-
-        Player(const Player &);
-
-        ~Player();
-
-        std::string getName();
-
-        int stacksize() const;
-
-        int cardesTaken() const;
-
-        void won(int);
-
-        void add_card(const Card &);
-
-        Card removeCard();
-
-        Card top_card() const;
-
-        Player &operator=(const Player &);
-
-        Player &operator=(Player &&) noexcept;
-    };
+Player::Player(std::string name) : name(std::move(name)), wonCards(0) {
+    if(this->name.length() == 0) this->name = "No Name";
 }
 
-#endif //CARDGAME_PLAYER_HPP
+Player::Player(const Player &other) : Player(other.name) {
+}
+
+Player::Player(Player &&other) noexcept: name(std::move(other.name)), stack(std::move(other.stack)),
+                                         wonCards(other.wonCards) {
+    other.wonCards = -1;
+}
+
+Player::~Player() = default;
+
+std::string Player::getName() {
+    return name;
+}
+
+int Player::stacksize() const {
+    return static_cast<int>(stack.size());
+}
+
+int Player::cardesTaken() const {
+    return wonCards;
+}
+
+void Player::won(int num_of_cards) {
+    wonCards += num_of_cards;
+}
+
+void Player::addCard(const Card &card) {
+    stack.push_back(card);
+}
+
+Card Player::removeCard() {
+    Card top = topCard();
+    stack.pop_back();
+    return top;
+}
+
+Card Player::topCard() const {
+    return stack.back();
+}
+
+Player &Player::operator=(const Player &other) {
+    if (this == &other) {
+        return *this;
+    }
+
+    name = other.name;
+    stack = other.stack;
+    wonCards = other.wonCards;
+
+    return *this;
+}
+
+Player &Player::operator=(Player &&other) noexcept {
+    if (this == &other) {
+        return *this;
+    }
+
+    name = std::move(other.name);
+    stack = std::move(other.stack);
+    wonCards = other.wonCards;
+    other.wonCards = -1;
+
+    return *this;
+}
+
